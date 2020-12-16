f1d5b6f0ee6797722caef165bb189a083c62e545
Khadija dh <khadija.dhieb@le-campus-numerique.fr>
2020-12-16 15:28:04 +0100

    port_configuration:_Ã8001

---
+++ src/main/java/com/DongeonsEtDragons/MS_interface/MsInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.DongeonsEtDragons.MS_interface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInterfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DongeonsEtDragons/MS_interface/MsInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DongeonsEtDragons.MS_interface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
